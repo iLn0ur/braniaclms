aa6b82814cbeb3e2f21b86e89d882b666c3f0e78
iLn0ur <you@example.com>
2022-12-02 18:56:31 +0300

    add site example

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class ContactView(TemplateView):
+    template_name = 'mainapp/contact.html'
+
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class HelloWorldView(View):
+#
+#     def get(self, request, *args, **kwargs):
+#         return HttpResponse('hello, world!')
+
+
+# def hello_world(request):
+#    return HttpResponse("hello")
+
+
+# def blog(request):
+#     return HttpResponse("i'm blog")
