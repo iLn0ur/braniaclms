80b3f2764473cb9143350e89782a8432aeff7d1a
iLn0ur <you@example.com>
2022-12-05 13:31:01 +0300

    fix html templates

--- braniaclms/settings.py
+++ braniaclms/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
+
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
 
     path('contacts/', views.ContactView.as_view()),
     path('courses/', views.CoursesListView.as_view()),
-    path('docsite', views.DocSiteView.as_view()),
+    path('docsite/', views.DocSiteView.as_view()),
     path('', views.IndexView.as_view()),
-    path('login', views.LoginView.as_view()),
-    path('news', views.NewsView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('news/', views.NewsView.as_view()),
 
 
 
